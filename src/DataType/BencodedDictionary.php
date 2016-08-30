63d0c124ae66d3be7704d78c47d826d216b9d26f
Ricardo Velhote <rvelhote@gmail.com>
2016-08-30 22:34:03 +0100

    ADD: Sort function on the dictionary itself

--- src/DataType/BencodedDictionary.php
+++ src/DataType/BencodedDictionary.php
@@ -68,6 +68,14 @@ class BencodedDictionary implements BencodedDataType, ArrayAccess, IteratorAggre
     }
 
     /**
+     *
+     */
+    public function sort()
+    {
+        ksort($this->value, SORT_STRING);
+    }
+
+    /**
      * Retrieve an external iterator
      * @link http://php.net/manual/en/iteratoraggregate.getiterator.php
      * @return Traversable An instance of an object implementing <b>Iterator</b> or
